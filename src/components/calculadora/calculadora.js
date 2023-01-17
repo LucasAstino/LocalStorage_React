8a05020809fb102f489f20731183eac441b5e03e
LUCAS ASTINO <lucasastino2@gmail.com>
2023-01-17 21:10:22 +0000

    Create calculadora.js and yarn.lock , criado components

---
+++ src/components/calculadora/calculadora.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class Calculadora extends React.Component {
+  constructor(props) {
+    super(props);
+    this.states = [
+      (acumulador = "0"),
+      (operador = "0"),
+      (valorTela = "0"),
+      (resultador = "0"),
+    ];
+    //Components
+    const tela = (valor, res) => {
+      return (
+        <div>
+          <span>{valor}</span>
+          <span>{res}</span>
+        </div>
+      );
+    };
+
+    const btn = (label, onclick) => {
+      return <button onclick={onclick}>{label}</button>;
+    };
+  }
+
+  // functions
+  
+}
