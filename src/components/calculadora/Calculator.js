b94b44b4ea38dbdd07f79fa9a7fea4b5e317f424
LUCAS ASTINO <lucasastino2@gmail.com>
2023-01-19 16:30:30 +0000

    Update Calculator.js and styleCalculator.css

--- src/components/calculadora/Calculator.js
+++ src/components/calculadora/Calculator.js
@@ -4,33 +4,44 @@ import "./styleCalculator.css";
 export default class Calculator extends React.Component {
   constructor() {
     super();
+    this.state = {
+      num: "5",
+    };
   }
+  addNum = (e) => {
+    // this.setState({ num: e.target.value });
+    console.log(e.target.value);
+  };
 
   render() {
     return (
       <div class="container">
-        <h1 class="display">0</h1>
+        <h1 class="display">{this.state.num}</h1>
         <section class="teclado">
           <button>AC</button>
           <button>-/+</button>
           <button>%</button>
-          <button>/</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>X</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>,</button>
-          <button>=</button>
+          <button class="orange">/</button>
+          <button onClick={(e) => this.addNum(e)} class="gray">
+            7
+          </button>
+          <button class="gray">8</button>
+          <button class="gray">9</button>
+          <button class="orange">X</button>
+          <button class="gray">4</button>
+          <button class="gray">5</button>
+          <button class="gray">6</button>
+          <button class="orange">-</button>
+          <button class="gray">1</button>
+          <button class="gray">2</button>
+          <button class="gray">3</button>
+          <button class="orange">+</button>
         </section>
+        <footer class="footer">
+          <button class="gray teste">0</button>
+          <button class="gray">,</button>
+          <button class="orange">=</button>
+        </footer>
       </div>
     );
   }
